8b672fc971c69268cfa95fca0a5657b1eaf9b856
EdwinLG1 <144741860+EdwinLG1@users.noreply.github.com>
2024-04-05 15:10:43 -0400

    Create ImageSliceTest.m
    Test for Image Slicing

---
+++ ImageSliceTest.m
@@ -0,0 +1,30 @@
+img = imread("Stitched_Image.png");
+
+figure(1),
+imshow(img);
+
+%Make distance a multiple of 8 for following code to work
+[M,N] = size(img,[1 2]);
+distance = 256;
+
+% make sure N/2 is an even number
+if mod(N,2) == 1
+    distance = distance + 1;
+end
+
+width = uint32(((N-distance)/2));
+
+img1 = uint8(zeros(M, width, 3));
+img2 = uint8(zeros(M, width, 3));
+
+
+img1(:, :, :) = img(:,1:width,:);
+for i = 1:width
+    img2(:, i, :) = img(:, i+width+distance, :);
+end
+
+figure(2),
+imshow(img1);
+
+figure(3),
+imshow(img2);
