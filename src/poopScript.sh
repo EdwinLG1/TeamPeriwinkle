05244b76a8e31b9eddf50547af4dd2d3d6f49a3f
aplano01 <87944830+aplano01@users.noreply.github.com>
2024-04-14 13:30:24 -0400

    Update poopScript.sh

--- src/poopScript.sh
+++ src/poopScript.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
 
-rm Mask.png
-rm Stitched_Image.png
-rm output/Stitched_Image.png
+echo "../input/$1" > image1.txt
+echo "../input/$2" > image2.txt
+
+#matlab -nosplash -nodesktop -r "run('ImagePreProcess_color.m');exit;"
 
 matlab -nosplash -nodesktop -r "run('ImageStitchMain.m');exit;"
 
@@ -10,7 +10,10 @@ while [ ! -f Mask.png ] || [ ! -f Stitched_Image.png ];
     do sleep 1
 done
 
-iopaint run --model=lama --device=cpu --image=./Stitched_Image.png --mask=./Mask.png --output=./output
+iopaint run --model=lama --device=cpu --image=./Stitched_Image.png --mask=./Mask.png --output=../output/$3
 
 rm Mask.png
 rm Stitched_Image.png
+
+rm image1.txt
+rm image2.txt
